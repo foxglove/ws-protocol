43f6699b7a194fd328c8ae80600b0b21c6819da0
Jacob Bandes-Storch <jacob@foxglove.dev>
2021-11-24 10:28:35 -0800

    Remove subscription and createDeserializer logic from TS client (#8)
    In order to reuse parsed descriptors for deserialization and other purposes, Studio needs to be in control of its descriptor cache. The logic embedded in this client was making that difficult. The client API is now more similar to the actual protocol.

--- typescript/scripts/example.ts
+++ typescript/scripts/example.ts
@@ -3,6 +3,7 @@ import protobufjs from "protobufjs";
 import { FileDescriptorSet } from "protobufjs/ext/descriptor";
 import WebSocket from "ws";
 
+import { SubscriptionId } from "../src";
 import FoxgloveClient from "../src/FoxgloveClient";
 
 async function main() {
@@ -12,19 +13,31 @@ async function main() {
   }
   const client = new FoxgloveClient({
     ws: new WebSocket(`ws://${host}:8765`, [FoxgloveClient.SUPPORTED_SUBPROTOCOL]),
-    createDeserializer: (channel) => {
+  });
+  const deserializers = new Map<SubscriptionId, (data: DataView) => unknown>();
+  client.on("advertise", (channels) => {
+    for (const channel of channels) {
       if (channel.encoding !== "protobuf") {
-        throw new Error(`Unsupported encoding ${channel.encoding}`);
+        console.warn(`Unsupported encoding ${channel.encoding}`);
       }
       const root = protobufjs.Root.fromDescriptor(
         FileDescriptorSet.decode(Buffer.from(channel.schema, "base64")),
       );
       const type = root.lookupType(channel.schemaName);
-      return (data) => type.decode(new Uint8Array(data.buffer, data.byteOffset, data.byteLength));
-    },
+
+      const subId = client.subscribe(channel.id);
+      deserializers.set(subId, (data) =>
+        type.decode(new Uint8Array(data.buffer, data.byteOffset, data.byteLength)),
+      );
+    }
   });
-  client.on("message", console.log);
-  client.subscribe(topic);
+  client.on("message", ({ subscriptionId, timestamp, data }) => {
+    console.log({
+      subscriptionId,
+      timestamp,
+      data: deserializers.get(subscriptionId)!(data),
+    });
+  });
 }
 
 main().catch(console.error);
