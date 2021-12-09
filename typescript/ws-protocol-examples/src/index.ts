73b7e01d56d3ee2f57b7a5e75303959ece53b0bf
Jacob Bandes-Storch <jacob@bandes-stor.ch>
2021-12-09 11:58:25 -0800

    Move TS examples to a separate package

---
+++ typescript/ws-protocol-examples/src/index.ts
@@ -0,0 +1,12 @@
+import { program } from "commander";
+
+import ImageServer from "./examples/image-server";
+import SimpleClient from "./examples/simple-client";
+import Sysmon from "./examples/sysmon";
+
+program.name("ws-protocol-examples");
+program.addCommand(ImageServer);
+program.addCommand(Sysmon);
+program.addCommand(SimpleClient);
+
+program.parseAsync().catch(console.error);
