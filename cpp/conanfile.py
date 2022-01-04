8a7e3d02bc2d3f6dc908cea5fda90d5407022f48
Jacob Bandes-Storch <jacob@bandes-stor.ch>
2022-01-03 18:21:27 -0800

    does make debug work?

--- cpp/conanfile.py
+++ cpp/conanfile.py
@@ -18,7 +18,6 @@ class FoxgloveWebSocketConan(ConanFile):
 
     def configure(self):
         self.options["websocketpp"].asio = "standalone"
-        self.options["compiler"].libcxx = "libc++"
 
     def source(self):
         self.run(f"git clone https://github.com/foxglove/ws-protocol.git")
