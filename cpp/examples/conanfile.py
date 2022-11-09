c1be7d65438fae9709215cdd7b7045fb1b791f30
Jacob Bandes-Storch <jacob@foxglove.dev>
2022-11-09 15:42:12 -0800

    Update python and c++ protobuf examples to demonstrate dynamic FileDescriptorSet generation (#269)
    **Public-Facing Changes**
    Updated example Python Protobuf server to demonstrate dynamic generation of FileDescriptorSet schemas.
    Updated example C++ server to use Protobuf instead of JSON, including dynamic generation of FileDescriptorSet schemas.
    
    **Description**
    Resolves https://github.com/foxglove/ws-protocol/issues/255
    Resolves #138
    Relates to https://github.com/foxglove/mcap/pull/694
    
    Checks in generated files for Python because we have not yet published them in a Python package (https://github.com/foxglove/schemas/issues/71).

--- cpp/examples/conanfile.py
+++ cpp/examples/conanfile.py
@@ -4,7 +4,7 @@ from conans import ConanFile, CMake
 class FoxgloveWebSocketExamplesConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
-    requires = "foxglove-websocket/0.0.1"
+    requires = "foxglove-websocket/0.0.1", "protobuf/3.21.1"
 
     def build(self):
         cmake = CMake(self)
--- cpp/examples/example_server.cpp
+++ cpp/examples/example_server.cpp
@@ -1,13 +1,14 @@
 #include <foxglove/websocket/server.hpp>
 
-#include <nlohmann/json.hpp>
+#include <google/protobuf/descriptor.pb.h>
+#include <google/protobuf/util/time_util.h>
 
 #include <atomic>
 #include <chrono>
 #include <iostream>
 #include <thread>
 
-using json = nlohmann::json;
+#include "foxglove/SceneUpdate.pb.h"
 
 static uint64_t nanosecondsSinceEpoch() {
   return uint64_t(std::chrono::duration_cast<std::chrono::nanoseconds>(
@@ -15,25 +16,80 @@ static uint64_t nanosecondsSinceEpoch() {
                     .count());
 }
 
+// Adapted from:
+// https://gist.github.com/tomykaira/f0fd86b6c73063283afe550bc5d77594
+// https://github.com/protocolbuffers/protobuf/blob/01fe22219a0312b178a265e75fe35422ea6afbb1/src/google/protobuf/compiler/csharp/csharp_helpers.cc
+static std::string Base64Encode(std::string_view data) {
+  constexpr const char ALPHABET[] =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+  std::string result;
+  // Every 3 bytes of data yields 4 bytes of output
+  result.reserve((data.size() + (3 - 1 /* round up */)) / 3 * 4);
+
+  size_t i = 0;
+  for (; i + 2 < data.size(); i += 3) {
+    result.push_back(ALPHABET[data[i] >> 2]);
+    result.push_back(ALPHABET[((data[i] & 0b11) << 4) | (data[i + 1] >> 4)]);
+    result.push_back(ALPHABET[((data[i + 1] & 0b1111) << 2) | (data[i + 2] >> 6)]);
+    result.push_back(ALPHABET[data[i + 2] & 0b111111]);
+  }
+  switch (data.size() - i) {
+    case 2:
+      result.push_back(ALPHABET[data[i] >> 2]);
+      result.push_back(ALPHABET[((data[i] & 0b11) << 4) | (data[i + 1] >> 4)]);
+      result.push_back(ALPHABET[(data[i + 1] & 0b1111) << 2]);
+      result.push_back('=');
+      break;
+    case 1:
+      result.push_back(ALPHABET[data[i] >> 2]);
+      result.push_back(ALPHABET[(data[i] & 0b11) << 4]);
+      result.push_back('=');
+      result.push_back('=');
+      break;
+  }
+
+  return result;
+}
+
+// Writes the FileDescriptor of this descriptor and all transitive dependencies
+// to a string, for use as a channel schema.
+static std::string SerializeFdSet(const google::protobuf::Descriptor* toplevelDescriptor) {
+  google::protobuf::FileDescriptorSet fdSet;
+  std::queue<const google::protobuf::FileDescriptor*> toAdd;
+  toAdd.push(toplevelDescriptor->file());
+  std::unordered_set<std::string> seenDependencies;
+  while (!toAdd.empty()) {
+    const google::protobuf::FileDescriptor* next = toAdd.front();
+    toAdd.pop();
+    next->CopyTo(fdSet.add_file());
+    for (int i = 0; i < next->dependency_count(); ++i) {
+      const auto& dep = next->dependency(i);
+      if (seenDependencies.find(dep->name()) == seenDependencies.end()) {
+        seenDependencies.insert(dep->name());
+        toAdd.push(dep);
+      }
+    }
+  }
+  return fdSet.SerializeAsString();
+}
+
+// https://danceswithcode.net/engineeringnotes/quaternions/quaternions.html
+static void setAxisAngle(foxglove::Quaternion* q, double x, double y, double z, double angle) {
+  double s = std::sin(angle / 2);
+  q->set_x(x * s);
+  q->set_y(y * s);
+  q->set_z(z * s);
+  q->set_w(std::cos(angle / 2));
+}
+
 int main() {
-  foxglove::websocket::Server server{8765, "example server"};
+  foxglove::websocket::Server server{8765, "C++ Protobuf example server"};
 
   const auto chanId = server.addChannel({
     .topic = "example_msg",
-    .encoding = "json",
-    .schemaName = "ExampleMsg",
-    .schema =
-      json{
-        {"type", "object"},
-        {
-          "properties",
-          {
-            {"msg", {{"type", "string"}}},
-            {"count", {{"type", "number"}}},
-          },
-        },
-      }
-        .dump(),
+    .encoding = "protobuf",
+    .schemaName = foxglove::SceneUpdate::descriptor()->full_name(),
+    .schema = Base64Encode(SerializeFdSet(foxglove::SceneUpdate::descriptor())),
   });
 
   server.setSubscribeHandler([&](foxglove::websocket::ChannelId chanId) {
@@ -43,16 +99,38 @@ int main() {
     std::cout << "last client unsubscribed from " << chanId << std::endl;
   });
 
-  uint64_t i = 0;
   std::shared_ptr<asio::steady_timer> timer;
   std::function<void()> setTimer = [&] {
-    timer = server.getEndpoint().set_timer(200, [&](std::error_code const& ec) {
+    timer = server.getEndpoint().set_timer(50, [&](std::error_code const& ec) {
       if (ec) {
         std::cerr << "timer error: " << ec.message() << std::endl;
         return;
       }
-      server.sendMessage(chanId, nanosecondsSinceEpoch(),
-                         json{{"msg", "Hello"}, {"count", i++}}.dump());
+
+      const auto now = nanosecondsSinceEpoch();
+
+      foxglove::SceneUpdate msg;
+      auto* entity = msg.add_entities();
+      *entity->mutable_timestamp() = google::protobuf::util::TimeUtil::NanosecondsToTimestamp(now);
+      entity->set_frame_id("root");
+      auto* cube = entity->add_cubes();
+      auto* size = cube->mutable_size();
+      size->set_x(1);
+      size->set_y(1);
+      size->set_z(1);
+      auto* position = cube->mutable_pose()->mutable_position();
+      position->set_x(2);
+      position->set_y(0);
+      position->set_z(0);
+      auto* orientation = cube->mutable_pose()->mutable_orientation();
+      setAxisAngle(orientation, 0, 0, 1, double(now) / 1e9 * 0.5);
+      auto* color = cube->mutable_color();
+      color->set_r(0.6);
+      color->set_g(0.2);
+      color->set_b(1);
+      color->set_a(1);
+
+      server.sendMessage(chanId, now, msg.SerializeAsString());
       setTimer();
     });
   };
