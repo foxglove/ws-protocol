70e95dbc737d63b24df1458a8347dd4cd3d0be97
John Hurliman <jhurliman@jhurliman.org>
2022-11-23 13:28:34 -0800

    [TypeScript] Add publish-client, support for TLS in simple-client (#283)

--- typescript/ws-protocol-examples/src/index.ts
+++ typescript/ws-protocol-examples/src/index.ts
@@ -3,13 +3,15 @@ import { program } from "commander";
 
 import ImageServer from "./examples/image-server";
 import McapPlay from "./examples/mcap-play";
+import PublishClient from "./examples/publish-client";
 import SimpleClient from "./examples/simple-client";
 import Sysmon from "./examples/sysmon";
 
 program.name("ws-protocol-examples");
 program.addCommand(ImageServer);
-program.addCommand(Sysmon);
-program.addCommand(SimpleClient);
 program.addCommand(McapPlay);
+program.addCommand(PublishClient);
+program.addCommand(SimpleClient);
+program.addCommand(Sysmon);
 
 program.parseAsync().catch(console.error);
