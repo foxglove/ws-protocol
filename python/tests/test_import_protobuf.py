1ab1a417818ee37bad9cc853e9ea52476e6585fd
Jacob Bandes-Storch <jacob@foxglove.dev>
2023-01-11 11:03:57 -0800

    remove outdated test

--- python/tests/test_import_protobuf.py
+++
@@ -1,7 +0,0 @@
-def test_import_protobuf():
-    """
-    Ensure the generated protobuf file is successfully importable in a dev environment.
-    """
-    from foxglove_websocket.examples.proto.foxglove.SceneUpdate_pb2 import SceneUpdate
-
-    _ = SceneUpdate()
