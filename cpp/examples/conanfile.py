383508bf7a01715da9b53169fbe9831c77638c80
Hans-Joachim Krauch <achim.krauch@gmail.com>
2023-02-24 13:08:40 -0300

    cpp: Update example server, migrate to conan2

--- cpp/examples/conanfile.py
+++ cpp/examples/conanfile.py
@@ -1,10 +1,24 @@
-from conans import ConanFile, CMake
-
+from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout
 
 class FoxgloveWebSocketExamplesConan(ConanFile):
+    name = "foxglove-websocket-example"
+    version = "0.0.1"
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake"
-    requires = "foxglove-websocket/0.0.1", "protobuf/3.21.4", "zlib/1.2.13"
+    exports_sources = "CMakeLists.txt", "src/*", "proto/*"
+    generators = "CMakeDeps"
+
+    def requirements(self):
+        self.requires("foxglove-websocket/0.0.1")
+        self.requires("protobuf/3.21.4")
+        self.requires("zlib/1.2.13")
+
+    def layout(self):
+        cmake_layout(self)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.generate()
 
     def build(self):
         cmake = CMake(self)
