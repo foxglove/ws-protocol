4b75ff2d2c36aed623f6e531fdef50b69479cabb
Hans-Joachim Krauch <achim-k@users.noreply.github.com>
2023-05-17 15:10:26 -0300

    Python: Disable compression by default, allow specifying server kwargs (#444)
    ### Public-Facing Changes
    
    - Python: Disable compression by default, allow specifying server kwargs
    
    ### Description
    Allows specifying [websockets.server.serve(...)
    ](https://websockets.readthedocs.io/en/stable/reference/asyncio/server.html#websockets.server.serve)keyword
    arguments and disables compression (`compression=None`) by default
    
    Fixes #443

--- python/src/foxglove_websocket/server/__init__.py
+++ python/src/foxglove_websocket/server/__init__.py
@@ -163,6 +163,7 @@ class FoxgloveServer:
         metadata: Optional[Mapping[str, str]] = None,
         session_id: Optional[str] = None,
         logger: logging.Logger = _get_default_logger(),
+        server_kwargs: Dict[str, Any] = {"compression": None},
     ):
         self.host = host
         self.port = port
@@ -178,6 +179,7 @@ class FoxgloveServer:
         self._next_service_id = ServiceId(0)
         self._subscribed_params = set([])
         self._logger = logger
+        self._server_kwargs = server_kwargs
         self._listener = None
         self._opened = asyncio.get_running_loop().create_future()
 
@@ -222,6 +224,7 @@ class FoxgloveServer:
                 self.host,
                 self.port,
                 subprotocols=[Subprotocol("foxglove.websocket.v1")],
+                **self._server_kwargs,
             )
             self._opened.set_result(server)
         except asyncio.CancelledError:
