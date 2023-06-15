44b4ca5978203a9f13d71611b93aea311f864adb
Jacob Bandes-Storch <jacob@foxglove.dev>
2023-06-14 18:10:41 -0700

    fixes, cleanup

--- cpp/examples/src/example_server_flatbuffers.cpp
+++ cpp/examples/src/example_server_flatbuffers.cpp
@@ -12,6 +12,7 @@
 #include <unordered_set>
 
 #include "SceneUpdate_generated.h"
+#include "base64.hpp"
 #include "flatbuffers/flatbuffers.h"
 
 namespace foxglove {
@@ -23,44 +24,6 @@ static uint64_t nanosecondsSinceEpoch() {
   return uint64_t(std::chrono::duration_cast<std::chrono::nanoseconds>(
                     std::chrono::system_clock::now().time_since_epoch())
                     .count());
-}
-
-// Adapted from:
-// https://gist.github.com/tomykaira/f0fd86b6c73063283afe550bc5d77594
-// https://github.com/protocolbuffers/protobuf/blob/01fe22219a0312b178a265e75fe35422ea6afbb1/src/google/protobuf/compiler/csharp/csharp_helpers.cc#L346
-static std::string Base64Encode(std::string_view input) {
-  constexpr const char ALPHABET[] =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-  std::string result;
-  // Every 3 bytes of data yields 4 bytes of output
-  result.reserve((input.size() + (3 - 1 /* round up */)) / 3 * 4);
-
-  // Unsigned values are required for bit-shifts below to work properly
-  const unsigned char* data = reinterpret_cast<const unsigned char*>(input.data());
-
-  size_t i = 0;
-  for (; i + 2 < input.size(); i += 3) {
-    result.push_back(ALPHABET[data[i] >> 2]);
-    result.push_back(ALPHABET[((data[i] & 0b11) << 4) | (data[i + 1] >> 4)]);
-    result.push_back(ALPHABET[((data[i + 1] & 0b1111) << 2) | (data[i + 2] >> 6)]);
-    result.push_back(ALPHABET[data[i + 2] & 0b111111]);
-  }
-  switch (input.size() - i) {
-    case 2:
-      result.push_back(ALPHABET[data[i] >> 2]);
-      result.push_back(ALPHABET[((data[i] & 0b11) << 4) | (data[i + 1] >> 4)]);
-      result.push_back(ALPHABET[(data[i + 1] & 0b1111) << 2]);
-      result.push_back('=');
-      break;
-    case 1:
-      result.push_back(ALPHABET[data[i] >> 2]);
-      result.push_back(ALPHABET[(data[i] & 0b11) << 4]);
-      result.push_back('=');
-      result.push_back('=');
-      break;
-  }
-
-  return result;
 }
 
 // https://danceswithcode.net/engineeringnotes/quaternions/quaternions.html
@@ -139,24 +102,31 @@ int main(int argc, char** argv) {
     const auto now = nanosecondsSinceEpoch();
     auto timestamp = foxglove::Time(now / 1'000'000'000, now % 1'000'000'000);
 
-    auto cubeBuilder = foxglove::CubePrimitiveBuilder(builder);
-    cubeBuilder.add_size(foxglove::CreateVector3(builder, 1, 1, 1));
-    cubeBuilder.add_pose(foxglove::CreatePose(
+    auto pose = foxglove::CreatePose(
       builder, foxglove::CreateVector3(builder, 2, 0, 0),
-      createQuaternionFromAxisAngle(builder, 0, 0, 1, double(now) / 1e9 * 0.5)));
-    cubeBuilder.add_color(foxglove::CreateColor(builder, 0.6, 0.2, 1, 1));
+      createQuaternionFromAxisAngle(builder, 0, 0, 1, double(now) / 1e9 * 0.5));
+    auto size = foxglove::CreateVector3(builder, 1, 1, 1);
+    auto color = foxglove::CreateColor(builder, 0.6, 0.2, 1, 1);
+    auto cubeBuilder = foxglove::CubePrimitiveBuilder(builder);
+    cubeBuilder.add_pose(pose);
+    cubeBuilder.add_size(size);
+    cubeBuilder.add_color(color);
     const auto cube = cubeBuilder.Finish();
+    builder.Finish(cube);
 
+    auto frameId = builder.CreateString("root");
+    auto cubes = builder.CreateVector({cube});
     auto entityBuilder = foxglove::SceneEntityBuilder(builder);
     entityBuilder.add_timestamp(&timestamp);
-    entityBuilder.add_frame_id(builder.CreateString("root"));
-    entityBuilder.add_cubes(builder.CreateVector({cube}));
+    entityBuilder.add_frame_id(frameId);
+    entityBuilder.add_cubes(cubes);
     const auto entity = entityBuilder.Finish();
+    builder.Finish(entity);
 
+    auto entities = builder.CreateVector({entity});
     auto updateBuilder = foxglove::SceneUpdateBuilder(builder);
-    updateBuilder.add_entities(builder.CreateVector({entity}));
+    updateBuilder.add_entities(entities);
     const auto update = updateBuilder.Finish();
-
     builder.Finish(update);
 
     server->broadcastMessage(chanId, now, builder.GetBufferPointer(), builder.GetSize());
